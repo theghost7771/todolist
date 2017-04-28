1f746f3375174eb3d6804f6e4e4adf14325e6b26
theghost7771 <theghost7771@gmail.com>
2017-04-28 14:11:46 +0300

    add readme, envexample, improove production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,3 +7,5 @@ from .base import *  # noqa
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 # Raises ImproperlyConfigured exception if DJANGO_SECRET_KEY not in os.environ
 SECRET_KEY = env('DJANGO_SECRET_KEY')
+
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['todo.emizee.com', ])
